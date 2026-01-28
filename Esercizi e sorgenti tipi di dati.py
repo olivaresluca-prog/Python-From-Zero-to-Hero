20b7a3bf8b101e8cd9a74b83bbfd838a440ed525
olivaresluca-prog <olivares.luca@gmail.com>
2026-01-28 17:03:03 +0100

    Add files via upload

---
+++ Esercizi e sorgenti tipi di dati.py
@@ -0,0 +1,16 @@
+print ("Esercizi e sorgenti tipi di dati")
+print ("Somma int =")
+x = 7
+y = 3
+print (x + y)
+print ("Moltiplicazione Float * Int =")
+prezzo = 19.99
+quantitá = 3
+print (prezzo * quantitá)
+print("Bolean =")
+numero = 10
+print (numero > 5) #true
+print ("Combinazione Int e Float =")
+a = 5
+b = 2.5 
+print (a + b)
